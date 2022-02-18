aaa5ec5ca762e20fe512676273418f2928806cac
sachin singh <46350715+swedsg@users.noreply.github.com>
2022-02-18 16:43:36 +0530

    Added webmgr project
    Added webmgr project to repo

---
+++ webmgr/src/main/java/com/izmo/webmgr/App.java
@@ -0,0 +1,13 @@
+package com.izmo.webmgr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
